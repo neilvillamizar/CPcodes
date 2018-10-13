3fca9bd6f8afdeb2c5ee3869faa6576b81681398
neilvillamizar <neilvillamizar@gmail.com>
2018-10-13 14:33:43 -0400

    Update BIT.cpp

--- OurCodes/BIT.cpp
+++ OurCodes/BIT.cpp
@@ -1,3 +1,4 @@
+//usar de 0  a n-1
 ll BIT[MAXN];
 ll sumBIT(int p){
     ll ret=0;
@@ -8,6 +9,6 @@ ll sumBIT(int p){
 }
 void updBIT(int p, ll val){
     p++;
-    for(; p<MAXN; p+=(p&-p))   //MOSCA con el limite MAXN (debe ser n+1)
+    for(; p<MAXN; p+=(p&-p))   //MOSCA con el limite MAXN (debe ser al menos n+1)
         BIT[p]+=val;
 }
